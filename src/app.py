d05feb87ca7379a4d17633c8f6710d3655101520
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-03-13 23:22:38 -0400

    created virtual environment

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.configapp.config['SQLALCHEMY_DATABASE_URI'] = \
+    "postgresql://admin:LbAGfF63trlyTzUF8ZgKvxO01k1pmsi6@" + \
+        "dpg-cg57dujhp8u9l205a1jg-a/tigerfocus_4ggq"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = '098098'
+
+db = SQLAlchemy(app)
+
+class People(db.Model):
+    
