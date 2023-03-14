805ad0ffaefa5490d51e260485c86b996f2ffec3
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-03-13 23:57:58 -0400

    initilaized test website for database

--- src/app.py
+++ src/app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
+from psycopg2 import connect
+
 
 app = Flask(__name__)
-app.configapp.config['SQLALCHEMY_DATABASE_URI'] = \
-    "postgresql://admin:LbAGfF63trlyTzUF8ZgKvxO01k1pmsi6@" + \
-        "dpg-cg57dujhp8u9l205a1jg-a/tigerfocus_4ggq"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = '098098'
-
-db = SQLAlchemy(app)
 
 
+
+@app.route("/")
+def index():
+    conn = connect(database="tigerfocus_4gqq", user="admin",
+            password="LbAGfF63trlyTzUF8ZgKvxO01k1pmsi6",
+            host="dpg-cg57dujhp8u9l205a1jg-a.ohio-postgres.render.com",
+            port="5432")
+            
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM users")
+        data = cur.fetchall()
+    
+    conn.close()
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
