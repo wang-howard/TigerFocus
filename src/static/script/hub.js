f8ea8d281b6eff8f02621586a59b1e134ed8bad2
Jad Bendarkawi <jadb@princeton.edu>
2023-03-15 17:17:46 -0400

    ui test js

--- src/static/script/hub.js
+++ src/static/script/hub.js
@@ -1,11 +1,9 @@
 function assignmentsTabClicked() {
-  var tabfirst = document.getElementById("assignments_tab");
-  var tabsecond = document.getElementById("preloaded_tab");
-  tabfirst.style.zindex = tabsecond.style.zIndex + 1;
+  var tab = document.getElementById("assignments_tab");
+  tab.style.zIndex++;
 }
 
 function preloadedTabClicked() {
-  var tabfirst = document.getElementById("preloaded_tab");
-  var tabsecond = document.getElementById("assignments_tab");
-  tabfirst.style.zindex = tabsecond.style.zIndex + 1;
+  var tab = document.getElementById("preloaded_tab");
+  tab.style.zIndex++;
 }
