afbc22f3f62afe15b62f7b533b66a1272266d181
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-03-14 23:42:27 -0400

    added reconnect in index error handling

--- src/app.py
+++ src/app.py
@@ -2,8 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from psycopg2 import connect
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder="./templates")
 
 hostname = os.environ.get("DB_HOST")
 connect_db = connect(
@@ -15,15 +14,29 @@ connect_db = connect(
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    with connect_db as conn:
-        print("Connecting to PostgreSQL database")
-        with conn.cursor() as cur:
-            print("Creating cursor")
-            cur.execute("SELECT * FROM users")
-            data = cur.fetchall()
-        print("Cursor closed")
-    print("Database connection closed")
-    return render_template("index.html", data=data)
+    try:
+        with connect_db as conn:
+            print("Connecting to PostgreSQL database")
+            with conn.cursor() as cur:
+                print("Creating cursor")
+                cur.execute("SELECT * FROM users")
+                data = cur.fetchall()
+            print("Cursor closed")
+        print("Database connection closed")
+        return render_template("index.html", data=data)
+    except Exception as ex:
+        print('{} - connection will be reset'.format(ex))
+        # Close old connection 
+        if conn:
+            if cursor:
+                cursor.close()
+            conn.close()
+        conn = None
+        cursor = None
+        
+        # Reconnect 
+        conn = connect_db
+        cursor = conn.cursor()
 
 @app.route("/hub")
 def hub():
