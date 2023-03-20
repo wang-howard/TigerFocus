2e5dbb48d4f06de216baef545c6e168c49f01f3f
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-03-20 11:59:57 -0400

    update new format

--- src/app.py
+++ src/app.py
@@ -1,24 +1,27 @@
+"""
+BEFORE RUNNING:
+    run the script below in terminal:
+    export DB_HOST=dpg-cg57dujhp8u9l205a1jg-a.ohio-postgres.render.com
+"""
+
 import psycopg2, os
 from flask import Flask, render_template, request, redirect, url_for
 
-
 app = Flask(__name__)
 
-hostname = "dpg-cg57dujhp8u9l205a1jg-a.ohio-postgres.render.com"
 connect_db = psycopg2.connect(
     database="tigerfocus_4gqq",
     user="admin",
     password="LbAGfF63trlyTzUF8ZgKvxO01k1pmsi6",
-    host=hostname,
+    host=os.environ["DB_HOST"],
     port="5432")
 
 @app.route("/", methods=["GET"])
 def index():
     try:
-        with connect_db as conn:
-            with conn.cursor() as cur:
-                cur.execute("SELECT * FROM users")
-                data = cur.fetchall()
+        with connect_db.cursor() as cur:
+            cur.execute("SELECT * FROM user_info")
+            data = cur.fetchall()
         return render_template("index.html", data=data)
     except Exception as ex:
         print(ex)
@@ -43,11 +46,22 @@ def created_user():
     last = request.form.get("last")
 
     try:
-        with connect_db as conn:
-            with conn.cursor() as cur:
-                query = "INSERT INTO users(user_id, first_name, last_name)"
-                query += "VALUES(%s, %s, %s)"
-                cur.execute(query, (id, first, last))
+        with connect_db.cursor() as cur:
+            query = "INSERT INTO users(user_id, first_name, last_name)"
+            query += "VALUES(%s, %s, %s)"
+            cur.execute(query, (id, first, last))
+        return redirect(url_for("index"))
+    except Exception as ex:
+        print(ex)
+        return render_template("error.html")
+
+@app.route("/selectuser", methods=["GET", "POST"])
+def select_user():
+    try:
+        with connect_db.cursor() as cur:
+            query = "SELECT PUID FROM user_info"
+            cur.execute(query)
+            ids = cur.fetchall()
         return redirect(url_for("index"))
     except Exception as ex:
         print(ex)
