f8e54e0b6434a90417dacc03dce115bbd38e3bb0
Jad Bendarkawi <jadb@princeton.edu>
2023-03-22 00:36:45 -0400

    unstyled create new window

--- src/static/script/hub.js
+++ src/static/script/hub.js
@@ -1,9 +1,26 @@
-function assignmentsTabClicked() {
-  var tab = document.getElementById("assignments_tab");
-  tab.style.zIndex++;
-}
+// Get the modal
+var modal = document.getElementById("create_new_modal");
 
-function preloadedTabClicked() {
-  var tab = document.getElementById("preloaded_tab");
-  tab.style.zIndex++;
-}
+// Get the button that opens the modal
+var btn = document.getElementById("create_new_button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+/* window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+ */
