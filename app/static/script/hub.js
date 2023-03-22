f3bef6dbef2da8fc436edbd7e0ca41ad7b6e4280
Jad Bendarkawi <jadb@princeton.edu>
2023-03-22 17:19:01 -0400

    ui

--- app/static/script/hub.js
+++ app/static/script/hub.js
@@ -1,26 +1,24 @@
 // Get the modal
-var modal = document.getElementById("create_new_modal");
+var create_modal = document.getElementById("create_new_modal");
+var course_modal = document.getElementById("add_course_modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("create_new_button");
+var create_btn = document.getElementById("create_new_button");
+var course_btn = document.getElementById("add_course_button");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
+create_btn.onclick = function () {
+  create_modal.style.display = "block";
+};
+
+course_btn.onclick = function () {
+  create_modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+  create_modal.style.display = "none";
 };
-
-// When the user clicks anywhere outside of the modal, close it
-/* window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
- */
