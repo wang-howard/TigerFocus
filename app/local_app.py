7b444d44ed46e5cbc58db3ba0f2ff92507635c21
wang-howard <105531941+wang-howard@users.noreply.github.com>
2023-03-29 18:19:33 -0400

    differentiated between local and deployment versions of application

--- app/local_app.py
+++ app/local_app.py
@@ -1,16 +1,19 @@
 """
 RUN THESE COMMANDS ON STARTUP:
-export FLASK_APP=app/app.py
+export FLASK_APP=app/local_app.py
 export DB_URI=postgresql://admin:LbAGfF63trlyTzUF8ZgKvxO01k1pmsi6@dpg-cg57dujhp8u9l205a1jg-a.ohio-postgres.render.com/tigerfocus_4gqq
 export SEC_KEY=tigerFocus098098
 """
 
 import os, enum, random
-from cas import CASClient
 from flask import Flask, request, session
 from flask import render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI")
@@ -19,13 +22,7 @@ app.config["SECRET_KEY"] = os.getenv("SEC_KEY")
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-cas_client = CASClient(
-    version=3,
-    service_url="http://tigerfocus.onrender.com/login?next=%2Fhub",
-    server_url="https://fed.princeton.edu/cas/"
-)
-
+Bootstrap = Bootstrap(app)
 
 """
 SQLAlchemy classes and enums
@@ -57,7 +54,7 @@ class Course(db.Model):
     def __repr__(self):
         return self.course_code + ": " + self.course_name + " (" +\
             str(self.id) + ")"
-    
+
 class Assignment(db.Model):
     __tablename__ = "assignments"
     id = db.Column(db.Integer, primary_key=True, unique=True)
@@ -75,39 +72,45 @@ View Functions
 @app.route("/", methods=["GET"])
 def index():
     try:
-        return render_template("index.html")
+        users = User.query.all()
+        user_data = []
+        if not users:
+            users = []
+        else:
+            for user in users:
+                user_data.append([user.netid, user.first_name,
+                                  user.last_name, user.user_type])
+        return render_template("index2.html", data=user_data)
     except Exception as ex:
         print(ex)
         return render_template("error.html", message=ex)
 
-@app.route("/login", methods=["GET"])
+# Simple Flask form
+class SelectIDForm(FlaskForm):
+    netid = SelectField("Select Net ID", coerce=str,
+                          validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     try:
-        if "netid" in session:
-            # already logged in
+        users = User.query.all()
+        netids = []
+        for user in users:
+            netids.append(user.netid)
+        form = SelectIDForm()
+        form.netid.choices = netids
+        if form.validate_on_submit():
+            session["netid"] = form.netid.data
             return redirect(url_for("hub"))
-
-        next = request.args.get("next")
-        ticket = request.args.get("ticket")
-        if not ticket:
-            # No ticket, request came from end user, send to CAS login
-            cas_login_url = cas_client.get_login_url()
-            print('CAS login URL: %s', cas_login_url)
-            return redirect(cas_login_url)
-        
-        print('ticket: %s', ticket)
-        print('next: %s', next)
-        user, _, _ = cas_client.verify_ticket(ticket)
-        if not user:
-            return render_template("error.html",
-                                   message="Failed to verify ticket")
-        else:
-            # Login successfully, redirect according "next" query parameter.
-            session['netid'] = user
-            return redirect(next)
+        return render_template("selectuser.html", form=form)
     except Exception as ex:
         print(ex)
         return render_template("error.html", message=ex)
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 @app.route("/newuser", methods=["POST"])
 def new_user():
@@ -118,13 +121,15 @@ def new_user():
 
     user = User(netid=netid, first_name=first, last_name=last,
                 user_type=user_type)
-    db.session.add(new_user)
+    db.session.add(user)
     db.session.commit()
+
+    session["netid"] = netid
     return redirect(url_for("hub"))
 
 @app.route("/hub")
 def hub():
-    netid = session["user_id"]
+    netid = session["netid"]
     try:
         user = User.query.filter_by(netid=netid).first()
         first = user.first_name
@@ -148,7 +153,7 @@ def hub():
         for a in assignments:
             course = Course.query.filter_by(id=a.course_id).first()
             assignment_data.append({"status": a.status,
-                                    "id": a.netid,
+                                    "id": a.id,
                                     "title": a.title,
                                     "due_date": a.due_date,
                                     "course_code":course.course_code,
@@ -160,47 +165,14 @@ def hub():
         print(ex)
         return render_template("error.html", message=ex)
 
-@app.route("/createduser", methods=["GET", "POST"])
-def created_user():
-    try:
-        user_id = request.form.get("userid")
-        first = request.form.get("first")
-        last = request.form.get("last")
-
-        new_user = User(netid=user_id, first_name=first, last_name=last,
-                        user_type=Role.student)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for("index"))
-    except Exception as ex:
-        print(ex)
-        return render_template("error.html", message=ex)
-
-@app.route("/selectuser", methods=["GET", "POST"])
-def select_user():
-    try:
-        users = User.query.all()
-        user_ids = []
-        for user in users:
-            user_ids.append(user.id)
-        form = SelectIDForm()
-        form.user_id.choices = user_ids
-        if form.validate_on_submit():
-            session["user_id"] = form.user_id.data
-            return redirect(url_for("hub"))
-        return render_template("selectuser.html", form=form)
-    except Exception as ex:
-        print(ex)
-        return render_template("error.html", message=ex)
-
 @app.route("/viewcourses", methods=["GET", "POST"])
 def view_courses():
-    user_id = session["user_id"]
+    netid = session["netid"]
     try:
         course_data = []
-        user = User.query.filter_by(id=user_id).first()
+        user = User.query.filter_by(id=netid).first()
         courses = user.courses
-        return render_template("courses.html", userid=user_id,
+        return render_template("courses.html", userid=netid,
                                courses=courses)
     except Exception as ex:
         print(ex)
@@ -220,7 +192,7 @@ def created_course():
         course_code = request.form.get("course_code")
         course_name = request.form.get("course_name")
         course_color = request.form.get("color")
-        user_id = session["user_id"]
+        netid = session["netid"]
         course_id = str(random.randint(0, 999999)).zfill(6)
         while True:
             query = Course.query.filter_by(id=course_id).first()
@@ -230,8 +202,8 @@ def created_course():
                 course_id = str(random.randint(0, 999999)).zfill(6)
         new_course = Course(id=course_id, course_code=course_code,
                             course_name=course_name, color=course_color,
-                            user_id=user_id)
-        user = User.query.filter_by(id=user_id).first()
+                            user_netid=netid)
+        user = User.query.filter_by(netid=netid).first()
         user.courses.append(new_course)
         db.session.add(new_course)
         db.session.commit()
@@ -247,14 +219,14 @@ def add_assignment():
         course = request.form.get("course_code")
         due = request.form.get("due_date")
         title = request.form.get("title")
-        user_id = session["user_id"]
+        netid = session["netid"]
         db_course = Course.query\
             .filter(Course.course_code==course)\
-            .filter(Course.user_id==user_id).first()
+            .filter(Course.user_netid==netid).first()
         course_id = db_course.id
         assignment_id = str(random.randint(0, 999999)).zfill(6)
         while True:
-            query = Assignment.query.filter_by(id=course_id).first()
+            query = Assignment.query.filter_by(id=assignment_id).first()
             if query == None:
                 break
             else:
@@ -273,10 +245,10 @@ def add_assignment():
 def delete_assignment():
     try:
         assignment = request.form.get("assignment_id")
-        user_id = session["user_id"]
+        netid = session["netid"]
         db_assignment = Course.query\
-            .filter(Course.assignment_id==assignment)\
-            .filter(Course.user_id==user_id).first()
+            .filter_by(assignment_id=assignment)\
+            .filter_by(user_netid=netid).first()
         
         db.session.delete(db_assignment)
         db.session.commit()
